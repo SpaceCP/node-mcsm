ccec6c546aec81ec7c5cacbc452d4b20754935b6
4mp3R <codwizard@gmail.com>
2015-04-11 13:41:53 +0200

    Code adjustments
    UTF encoding/decoding library introduced via external dependencies in
    order to make the code cleaner, safer and more readable.
    1.5 and 1.6 clients tested, everything seems to work as it should.

--- test.js
+++ test.js
@@ -1,27 +1,29 @@
+
 var MCServerMessager = require('./lib/MCServerMessager');
 
 var mcsm = new MCServerMessager({
 	host: "0.0.0.0", // Listen on all interfaces
 	message: "§4§lThe server will be up in an hour. Grab a coffe :P",
-	motd: "WORK IN PROGRESS",
 	players: "0",
-	maxPlayers: "64"
+	maxPlayers: "58",
+
+	reportedVersion: "1.5.1",	//MC client version
+	protocolVersion: "60",		//MC protocol version
+
+	//reportedVersion: "1.6.2",
+	//protocolVersion: "74",
+	
 });
 
 mcsm.start();
 
 setTimeout(function() {
+	mcsm.stop();
+	mcsm.setOption("message", "Just give me five more minutes...");
+	mcsm.setOption("motd", "Almost up :D");
 	mcsm.start();
-}, 2000);
+}, 3300000);
 
 setTimeout(function() {
 	mcsm.stop();
-}, 10000);
-
-setTimeout(function() {
-	mcsm.stop();
-}, 11000);
-
-setTimeout(function() {
-	mcsm.start();
-}, 15000);
+}, 3600000);
