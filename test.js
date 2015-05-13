f7d8e4dc8da97a51d2b37d60768810eccb60e4ba
4mp3R <codwizard@gmail.com>
2015-05-13 20:44:05 +0200

    Status ping partially implemented for v>=1.7

--- test.js
+++ test.js
@@ -5,13 +5,17 @@ var mcsm = new MCServerMessager({
 	host: "0.0.0.0", // Listen on all interfaces
 	message: "§4§lThe server will be up in an hour. Grab a coffe :P",
 	players: "0",
-	maxPlayers: "58",
+	maxPlayers: "48",
+	motd: "Yaaay it almost works",
 
-	reportedVersion: "1.5.1",	//MC client version
-	protocolVersion: "60",		//MC protocol version
+	//reportedVersion: "1.5.1",	//MC client version
+	//protocolVersion: "60",		//MC protocol version
 
 	//reportedVersion: "1.6.2",
 	//protocolVersion: "74",
+
+	reportedVersion: "1.8.3",
+	protocolVersion: "47"
 	
 });
 
